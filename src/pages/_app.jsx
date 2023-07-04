c0976e313ab91dd8774a6201f233606b5ff150c5
Somnath Pawar <samlpawar@gmail.com>
2023-07-04 16:58:44 +0530

    Under Construction

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -27,11 +27,11 @@ export default function App({ Component, pageProps, router }) {
         </div>
       </div>
       <div className="relative">
-        <Header />
-        <main>
+        {/* <Header /> */}
+        <main className="h-screen">
           <Component previousPathname={previousPathname} {...pageProps} />
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
